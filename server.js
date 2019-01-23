54172c64e534f456b77ec4f0786eea2b96f40f2a
Konenkamp <n0267144@lm.lmig.com>
2019-01-22 21:19:33 -0800

    configure setup check in

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-// //2. Make a package.json file by running `npm init` from the command line.
-
-// 3. Install the Express npm package: `npm install express`.
-
-
-// 5. Install the Handlebars npm package: `npm install express-handlebars`.
-
-// 6. Install MySQL npm package: `npm install mysql`.
-
-// 7. Require the following npm packages inside of the server.js file:
-//    * express
+var express = require('express');
