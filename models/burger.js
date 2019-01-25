8ebab0e57de597152d439cacbd54b611b778949b
Konenkamp <n0267144@lm.lmig.com>
2019-01-24 21:25:01 -0800

    adding orm and burger js

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+    update: function (objColVals, condtion, cb) {
+        orm.update("burgers", condition, function(res){
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
